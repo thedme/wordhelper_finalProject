7df760aae536e5c5a227172b190cb4c252b3bf41
Dovid Eskenazi <dvdsknz@gmail.com>
2016-10-11 10:08:52 -0400

    first-commit

---
+++ startbootstrap-freelancer-gh-pages/js/app.js
@@ -0,0 +1,9 @@
+$(function() {
+    // Get the form.
+    var form = $('#ajax-contact');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
+    // TODO: The rest of the code will go here...
+});
